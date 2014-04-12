0739a5cca8c0693837fc6a0f22b64d1fb8992b9b
ogcraft <ogcraft@gmail.com>
2014-04-12 21:12:20 +0300

    ver.0.1

---
+++ mooviefishapp/src/com/moviefish/mooviefishapp/CustomListViewAdapter.java
@@ -0,0 +1,55 @@
+package com.moviefish.mooviefishapp;
+ 
+import java.util.List;
+import com.moviefish.mooviefishapp.R;
+import com.moviefish.mooviefishapp.RowItem;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+ 
+public class CustomListViewAdapter extends ArrayAdapter<RowItem> {
+ 
+    Context context;
+ 
+    public CustomListViewAdapter(Context context, int resourceId,
+            List<RowItem> items) {
+        super(context, resourceId, items);
+        this.context = context;
+    }
+ 
+    /*private view holder class*/
+    private class ViewHolder {
+        ImageView imageView;
+        TextView txtTitle;
+        TextView txtDesc;
+    }
+ 
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        RowItem rowItem = getItem(position);
+ 
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item, null);
+            holder = new ViewHolder();
+            holder.txtDesc = (TextView) convertView.findViewById(R.id.desc);
+            holder.txtTitle = (TextView) convertView.findViewById(R.id.title);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.icon);
+            convertView.setTag(holder);
+        } else
+            holder = (ViewHolder) convertView.getTag();
+ 
+        holder.txtDesc.setText(rowItem.desc);
+        holder.txtTitle.setText(rowItem.title);
+        //Log.d(TAG,"img uri: " +)
+        holder.imageView.setImageURI(rowItem.getImgUri());
+ 
+        return convertView;
+    }
+}
