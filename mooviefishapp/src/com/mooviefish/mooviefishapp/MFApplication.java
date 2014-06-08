e11682ba56f8d68b3c97a02e85e5682c652997b7
ogcraft <ogcraft@gmail.com>
2014-06-08 15:37:12 +0300

    added requestWebService()

--- mooviefishapp/src/com/mooviefish/mooviefishapp/MFApplication.java
+++ mooviefishapp/src/com/mooviefish/mooviefishapp/MFApplication.java
@@ -18,10 +18,15 @@ import org.json.JSONException;
 import android.util.Log; 
 import java.util.*;
 import java.util.regex.*;
+import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import android.text.TextUtils;
 import android.os.Build;
 import com.mooviefish.mooviefishapp.Amatch;
-
+import java.net.URL;
 /**
 * This is a global state for MoovieFishApp
 *
@@ -44,6 +49,8 @@ public class MFApplication extends Application
 	private String root_path = Environment.getExternalStorageDirectory() + "/MoovieFish/";
 	public static Amatch amatch = null;
     private static final Pattern DIR_SEPARATOR = Pattern.compile("/");
+    static int CONNECTION_TIMEOUT = 0; //msec
+    static int DATARETRIEVAL_TIMEOUT = 0; //msec
 
 	List<MovieItem> movieItems;
 
@@ -170,6 +177,64 @@ try {
 
     // return JSON String
     return jsonarr;  //<<<< return JSONArray instead of JSONObject
+}
+
+public static InputStream requestWebService(String serviceUrl) {
+    
+    disableConnectionReuseIfNecessary();
+ 
+    HttpURLConnection urlConnection = null;
+    try {
+        // create connection
+        URL urlToRequest = new URL(serviceUrl);
+        urlConnection = (HttpURLConnection) 
+            urlToRequest.openConnection();
+        urlConnection.setConnectTimeout(CONNECTION_TIMEOUT);
+        urlConnection.setReadTimeout(DATARETRIEVAL_TIMEOUT);
+         
+        // handle issues
+        int statusCode = urlConnection.getResponseCode();
+        if (statusCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
+            // handle unauthorized (if service requires user login)
+        } else if (statusCode != HttpURLConnection.HTTP_OK) {
+            // handle any other errors, like 404, 500,..
+        }
+         
+        // create JSON object from content
+        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        //return new JSONObject(getResponseText(in));
+        return in;
+         
+    } catch (MalformedURLException e) {
+        // URL is invalid
+    } catch (SocketTimeoutException e) {
+        // data retrieval or connection timed out
+    } catch (IOException e) {
+        // could not read response body 
+        // (could not create input stream)
+    } finally {
+        if (urlConnection != null) {
+            urlConnection.disconnect();
+        }
+    }           
+    return null;
+}
+ 
+/**
+ * required in order to prevent issues in earlier Android version.
+ */
+private static void disableConnectionReuseIfNecessary() {
+    // see HttpURLConnection API doc
+    if (Integer.parseInt(Build.VERSION.SDK) 
+            < Build.VERSION_CODES.FROYO) {
+        System.setProperty("http.keepAlive", "false");
+    }
+}
+ 
+private static String getResponseText(InputStream inStream) {
+    // very nice trick from 
+    // http://weblogs.java.net/blog/pat/archive/2004/10/stupid_scanner_1.html
+    return new Scanner(inStream).useDelimiter("\\A").next();
 }
 
 
